8cb88059d7743dda3bac8ec6580905df4bcd2956
BIT Student <classroom@bgit.rs>
2020-02-12 17:29:02 +0100

    Arrays&Switch

---
+++ conditions/Switch&Arrays.js
@@ -0,0 +1,271 @@
+//////////////////////////////////////PRVI
+/*
+var a = 3;
+var res = "";
+
+switch (a) {
+    case 1:
+        res = "Monday";
+        break;
+    case 2:
+        res = "Tuesday";
+        break;
+    case 3:
+        res = "Wednesday";
+        break;
+    case 4:
+        res = "Thursday"
+        break;
+    case 5:
+        res = "Friday";
+        break;
+    case 6:
+        res = "Saturday";
+        break;
+    case 7:
+        res = "Sunday";
+        break;
+    default:
+        res = "Not a number";
+}
+console.log(res)
+*/
+/////////////////////////////////////////////DRUGI
+// var a = 13;
+// var res = "";
+
+// switch (a) {
+//     case 1:
+//         res = "Monday";
+//         break;
+//     case 2:
+//         res = "Tuesday";
+//         break;
+//     case 3:
+//         res = "Wednesday";
+//         break;
+//     case 4:
+//         res = "Thursday"
+//         break;
+//     case 5:
+//         res = "Friday";
+//         break;
+//     case 6:
+//         res = "Saturday";
+//         break;
+//     case 7:
+//         res = "Sunday";
+//         break;
+//     default:
+//         res = "Input must be a number between 1 and 7";
+// }
+// console.log(res);
+//////////////////////////////////////////////TRECI
+// var a = 8;
+// var res = "";
+
+// switch (a) {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         res = "May";
+//         break;
+
+//     caseJune    case 7:
+//         res = "";
+//         break;
+//     default:
+//         res = "Input must be number between 1 and 7";
+// }
+// console.log(res);
+//////////////////////////////////////////////////////CETVRTI
+// var a = 6;
+// var res = "";
+
+// switch (a) {
+//     case 1:
+//         res = "January";
+//         break;
+//     case 2:
+//         res = "February";
+//         break;
+//     case 3:
+//         res = "March";
+//         break;
+//     case 4:
+//         res = "April"
+//         break;
+//     case 5:
+//         res = "May";
+//         break;
+//     case 6:
+//         res = "June";
+//         break;
+//     case 7:
+//         res = "July";
+//         break;
+//     case 8:
+//         res = "August";
+//         break;
+//     case 9:
+//         res = "September";
+//         break;
+//     case 10:
+//         res = "October";
+//         break;
+//     case 11:
+//         res = "November";
+//         break;
+//     case 12:
+//         res = "December";
+//         break;
+
+//     default:
+//         res = "Input must be number between 1 and 12";
+// }
+// console.log(res);
+//////////////////////////////////////////////////PETI
+// var a = 1;
+// var res = "";
+
+// switch (a) {
+//     case 1:
+//     case 2:
+//     case 3:
+//         res = "Zima";
+//         break;
+//     case 4:
+//     case 5:
+//     case 6:
+//         res = "Prolece";
+//         break;
+//     case 7:
+//     case 8:
+//     case 9:
+//         res = "Leto";
+//         break;
+//     case 10:
+//     case 11:
+//     case 12:
+//         res = "Jesen";
+//         break;
+//     default:
+//         res = "Input must be number between 1 and 12"
+// }
+// console.log(res);
+////////////////////////////////////////SESTI
+// var a = "B";
+// var res = "";
+// switch (a) {
+//     case "a":
+//     case "A":
+//         res = "GOOD JOB!!";
+//         break;
+//     case "b":
+//     case "B":
+//         res = "PRETTY GOOD!!!";
+//         break;
+//     case "c":
+//     case "C":
+//         res = "PASSED!!";
+//         break;
+//     case "d":
+//     case "D":
+//         res = "NOT SO GOOD!!";
+//         break;
+//     case "f":
+//     case "F":
+//         res = "FAILED :(";
+//         break;
+//     default:
+//         res = "UNKNOWN GRADE!!";
+
+// }
+// console.log(res);
+//////////////////////////////////SEDMI
+// var city = "Segedin";
+// var res = "";
+
+// switch (city) {
+//     case "Beograd":
+//     case "Nis":
+//     case "Novi Sad":
+//         res = "Srbija";
+//         break;
+//     case "Sarajevo":
+//     case "Mostar":
+//     case "Banja Luka":
+//         res = "Bosna";
+//         break;
+//     case "Zagreb":
+//     case "Rijeka":
+//     case "Dubrovnik":
+//         res = "Hrvatska";
+//         break;
+//     case "Skoplje":
+//     case "Bitolj":
+//     case "Kumanovo":
+//         res = "Makedonija";
+//         break;
+//     case "Podgorica":
+//     case "Herceg Novi":
+//     case "Bar":
+//         res = "Crna Gora";
+//         break;
+//     default:
+//         res = "Pogresan dgra!";
+
+// }
+
+// console.log(res);
+////////////////////////////////////////////////////////////////////////////////////////////////OSMI
+// var a = 6;
+// var b = 0;
+// var c = "/";
+// var res = "";
+// var x;
+
+// switch (c) {
+//     case "+":
+//         x = a + b;
+//         res = "Resultat je : " + x;
+//         break;
+//     case "-":
+//         x = a - b;
+//         res = "Resultat je : " + x;
+//         break;
+//     case "*":
+//         x = a * b;
+//         res = "Resultat je : " + x;
+//         break;
+//     case "/":
+//         if (b === 0) {
+//             res = "Deljenje sa 0 nije moguce"
+//         } else {
+//             x = a / b;
+//             res = "Resultat je : " + x;
+//         }
+//         break;
+//     default:
+//         res = "Uneli ste pogresan operater";
+
+// }
+// console.log(res);
+//////////////////////////////////////ARRAYS 1
+// var a = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// console.log(a[5], a[9], a[0]);
+///////////////////////////////////////ARRAYS 2
+// var a = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+// console.log(a[6]);
+////////////////////////////////////////// ARRAYS3
+// var a = [2, -4, 5, -2, -11];
+// console.log(a[1], a[3], a[4]);
+///////////////////////////////////////// ARRAYS4
+// var a = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+// console.log(a[1], a[4], a[5], a[6], a[8]);
+//////////////////////////////////////////////ARRAYS 5
+// var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+// console.log(a[2][1]);
+
